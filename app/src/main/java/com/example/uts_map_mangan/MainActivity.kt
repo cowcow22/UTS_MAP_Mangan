30ade6de23e258cd912dc4009e1162544c658670
Jacques Farrell <129415702+Bangjekkk@users.noreply.github.com>
2024-10-08 20:02:53 +0700

    Update WelcomePageName
    lengkap tp baru mentahan input field, blm dimasukin ke database

--- app/src/main/java/com/example/uts_map_mangan/MainActivity.kt
+++ app/src/main/java/com/example/uts_map_mangan/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.uts_map_mangan
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Menangani klik tombol "Create a New Account"
+        val btnCreateNewAccount = findViewById<Button>(R.id.btn_create_new_account)
+        btnCreateNewAccount.setOnClickListener {
+            // Membuat Intent untuk membuka WelcomePageNameActivity
+            val intent = Intent(this, WelcomePageNameActivity::class.java)
+            startActivity(intent)
         }
     }
 }
