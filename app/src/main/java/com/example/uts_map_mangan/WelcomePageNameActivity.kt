ef348aa515db7e6668e46ee5071fc699bf1fc1e1
Jacques Farrell <129415702+Bangjekkk@users.noreply.github.com>
2024-10-09 23:19:19 +0700

    Update Page Goals
    masih manual, blm bisa read @drawable bjir aneh cok

--- app/src/main/java/com/example/uts_map_mangan/WelcomePageNameActivity.kt
+++ app/src/main/java/com/example/uts_map_mangan/WelcomePageNameActivity.kt
@@ -1,6 +1,8 @@
 package com.example.uts_map_mangan
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,14 @@ class WelcomePageNameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.welcome_page_name) // Pastikan layout ini ada
+        setContentView(R.layout.welcome_page_name)
+
+        // Menangani klik tombol "Create a New Account"
+        val btnGoNext = findViewById<Button>(R.id.btn_go_next)
+        btnGoNext.setOnClickListener {
+            // Membuat Intent untuk membuka WelcomePageNameActivity
+            val intent = Intent(this, WelcomePageGoalsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
