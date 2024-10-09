c9713606c436912d5c3d076673040cc988fe0502
Jacques Farrell <129415702+Bangjekkk@users.noreply.github.com>
2024-10-09 23:56:12 +0700

    Update Welcome Page until Gender Page
    masih blm selesai yang gender, blm ada kotlinnya juga

---
+++ app/src/main/java/com/example/uts_map_mangan/WelcomePageHeightActivity.kt
@@ -0,0 +1,15 @@
+package com.example.uts_map_mangan
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class WelcomePageHeightActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome_page_height)
+    }
+}
--- app/src/main/java/com/example/uts_map_mangan/WelcomePageWeightActivity.kt
+++ app/src/main/java/com/example/uts_map_mangan/WelcomePageWeightActivity.kt
@@ -8,17 +8,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class WelcomePageGoalsActivity : AppCompatActivity() {
+class WelcomePageWeightActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.welcome_page_goals)
+        setContentView(R.layout.welcome_page_weight)
 
         // Menangani klik tombol "Create a New Account"
         val btnGoNext = findViewById<Button>(R.id.btn_go_next)
         btnGoNext.setOnClickListener {
             // Membuat Intent untuk membuka WelcomePageNameActivity
-            val intent = Intent(this, WelcomePageWeightActivity::class.java)
+            val intent = Intent(this, WelcomePageHeightActivity::class.java)
             startActivity(intent)
         }
     }
