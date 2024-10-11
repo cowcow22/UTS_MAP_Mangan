a2c3ca67c7b98e0248387bb4e45ec6b7f9b04795
Jacques Farrell <129415702+Bangjekkk@users.noreply.github.com>
2024-10-11 23:38:37 +0700

    Update button gender animation
    new gender button

--- app/src/main/java/com/example/uts_map_mangan/WelcomePageGenderActivity.kt
+++ app/src/main/java/com/example/uts_map_mangan/WelcomePageGenderActivity.kt
@@ -1,15 +1,42 @@
 package com.example.uts_map_mangan
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class WelcomePageGenderActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_page_gender)
+
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroupGender)
+
+        // Menangani perubahan pemilihan RadioButton
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            // Reset semua RadioButton
+            resetRadioButtons()
+
+            // Temukan RadioButton yang dipilih
+            val selectedButton = findViewById<RadioButton>(checkedId)
+            when (checkedId) {
+                R.id.btn_male -> {
+                    selectedButton.setBackgroundResource(R.drawable.male_button_selected)
+                }
+                R.id.btn_female -> {
+                    selectedButton.setBackgroundResource(R.drawable.female_button_selected)
+                }
+            }
+        }
+    }
+
+    private fun resetRadioButtons() {
+        val maleButton = findViewById<RadioButton>(R.id.btn_male)
+        val femaleButton = findViewById<RadioButton>(R.id.btn_female)
+
+        // Kembalikan ke latar belakang default
+        maleButton.setBackgroundResource(R.drawable.ic_male)
+        femaleButton.setBackgroundResource(R.drawable.ic_female)
     }
 }
