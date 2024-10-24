eea44526b8a276a95286086321f9424b3b0a1683
Felyks <gamingfelix78@gmail.com>
2024-10-24 12:46:50 +0700

    Tampilan Account Management

--- app/src/main/java/com/example/uts_map_mangan/AccountManagementSetting.kt
+++ app/src/main/java/com/example/uts_map_mangan/AccountManagementSetting.kt
@@ -4,19 +4,18 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class GeneralSettingsActivity : AppCompatActivity() {
-
+class AccountManagementSetting : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.general_settings) // Updated to use general_settings.xml
+        setContentView(R.layout.activity_account_management_setting) // Updated to use activity_account_management_setting.xml
 
         findViewById<Button>(R.id.back_button).setOnClickListener {
             onBackPressed()
-        }
-        findViewById<Button>(R.id.account_management_button).setOnClickListener {
-            startActivity(Intent(this, AccountManagementSetting::class.java))
         }
     }
 
