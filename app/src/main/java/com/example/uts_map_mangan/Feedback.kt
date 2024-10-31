3dab3d48b1949f6ca85711dc843f529ad61acb70
Felyks <gamingfelix78@gmail.com>
2024-10-31 12:49:43 +0700

    Feedback feature is functional
    creating feedback to firebase

---
+++ app/src/main/java/com/example/uts_map_mangan/Feedback.kt
@@ -0,0 +1,7 @@
+package com.example.uts_map_mangan
+
+data class Feedback(
+    val categoryFeedback: String,
+    val textFeedback: String,
+    val userID: String
+)
--- app/src/main/java/com/example/uts_map_mangan/FeedbackSettingsActivity.kt
+++ app/src/main/java/com/example/uts_map_mangan/FeedbackSettingsActivity.kt
@@ -3,15 +3,72 @@ package com.example.uts_map_mangan
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class FeedbackSettingsActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.feedback_activity) // Updated to use activity_remove_ads.xml
+        setContentView(R.layout.feedback_activity)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        val feedbackRadioGroup = findViewById<RadioGroup>(R.id.feedback_radio_group)
+        val textEditText = findViewById<EditText>(R.id.feedback_text)
+
+        val submitButton = findViewById<Button>(R.id.submitButton)
+
+        submitButton.setOnClickListener {
+            val selectedRadioButtonId = feedbackRadioGroup.checkedRadioButtonId
+            if (selectedRadioButtonId != -1) {
+                val selectedRadioButton = findViewById<RadioButton>(selectedRadioButtonId)
+                val category = selectedRadioButton.text.toString()
+                val text = textEditText.text.toString()
+                if (text.isNotEmpty()) {
+                    submitFeedback(category, text)
+                } else {
+                    Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Please select a category", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         findViewById<Button>(R.id.back_button).setOnClickListener {
             onBackPressed()
+        }
+    }
+
+    private fun submitFeedback(category: String, text: String) {
+        val user = firebaseAuth.currentUser
+        user?.uid?.let { uid ->
+            val feedback = Feedback(category, text, uid)
+            firestore.collection("feedbacks")
+                .add(feedback)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Feedback submitted successfully", Toast.LENGTH_SHORT)
+                        .show()
+                    finish()
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(
+                        this,
+                        "Error submitting feedback: ${e.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+        } ?: run {
+            Toast.makeText(this, "User not logged in", Toast.LENGTH_SHORT).show()
         }
     }
 
